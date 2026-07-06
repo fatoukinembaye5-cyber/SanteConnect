d56b4eca3b440e7e881105179c06fe20719a87da
fatoukinembaye5-cyber <fatoukine.mbaye5@unchk.edu.sn>
2026-07-06 16:10:45 +0000

    restart

--- resources/js/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import LoginPage from './components/LoginPage';
-
-ReactDOM.createRoot(document.getElementById('app')).render(
-  <React.StrictMode>
-    <LoginPage />
-  </React.StrictMode>
-);
