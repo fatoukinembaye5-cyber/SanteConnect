4605d1e49390bff8540691cf89d050f7b3c2fc1f
serignesamb-crea <serigne.samb@unchk.edu.sn>
2026-07-09 18:29:12 +0200

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-// App.jsx
-import React from 'react';
-import AppRouter from './routes/AppRouter';
-import './index.css'; // Assure-toi que Tailwind est bien importé ici
+import "./index.css"; // Assure-toi que tailwind est bien importé ici
+import AdminDashboard from "./Dashboard/AdminDashboard.jsx";
+// Si vous utilisez un système de routes à l'avenir, importez AppRouter ici
 
 function App() {
   return (
-    <AppRouter />
+    <>
+      {/* Vous pouvez basculer entre <AppRouter /> ou <AdminDashboard /> selon vos besoins réels */}
+      <AdminDashboard />
+    </>
   );
+  
 }
 
 export default App;
